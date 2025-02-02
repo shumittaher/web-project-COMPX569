896261cf9d00bd2b751c9fb628bb0fe7c64493ee
Shumit Taher <Shumitt@gmail.com>
2025-02-03 12:22:54 +1300

    made a log in for guest users trying to post

--- modules/database.js
+++ modules/database.js
@@ -4,11 +4,14 @@ const USER_NAME = process.env.USER_NAME;
 const USER_PASS = process.env.USER_PASS;
 const HOST = process.env.HOST;
 
-const database = mariadb.createConnection({
+const database = mariadb.createPool({
     host: HOST,
     database: USER_NAME,
     user: USER_NAME,
-    password: USER_PASS
+    password: USER_PASS,
+    connectionLimit: 10,
+    idleTimeout: 60000,
+    acquireTimeout: 10000,
 });
 
 module.exports = database;

ecda2813efd4bd81e8031f2990e2fac9402deb81
Shumit Taher <Shumitt@gmail.com>
2025-02-03 20:07:55 +1300

    added a lieks table

--- init-db.sql
+++ init-db.sql
@@ -28,4 +28,16 @@ CREATE TABLE IF NOT EXISTS project_article_parents
     FOREIGN KEY (article_id) REFERENCES project_articles(id) ON DELETE CASCADE,
     FOREIGN KEY (parent_article_id) REFERENCES project_articles(id) ON DELETE CASCADE,
     PRIMARY KEY (article_id, parent_article_id)
+);
+
+CREATE TABLE IF NOT EXISTS project_article_likes
+(
+    article_id int not null,
+    userid     int not null,
+    FOREIGN KEY (article_id) REFERENCES project_articles (id) ON DELETE CASCADE,
+    FOREIGN KEY (userid) REFERENCES project_users (id) ON DELETE CASCADE,
+    PRIMARY KEY (article_id, userid)
 )
+
+
+

6441077a96bf6bdf2326b9a7197311696abb063b
Shumit Taher <Shumitt@gmail.com>
2025-02-05 01:35:22 +1300

    moving temp folder outside route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -7,7 +7,7 @@ const path = require("path");
 const fs = require("fs");
 const multer = require("multer");
 const upload = multer({
-    dest: path.join(__dirname, "temp")
+    dest: path.join(__dirname, "..","temp")
 });
 const {Jimp} = require("jimp");
 

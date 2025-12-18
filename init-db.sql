aab46e8c02dc66cf48abda34aca3994ac2ef9c97
Shumit Taher <shumitt@gmail.com>
2025-12-19 10:53:45 +1300

    updated sql initialization

--- init-db.sql
+++ init-db.sql
@@ -9,15 +9,28 @@ CREATE TABLE IF NOT EXISTS project_users
     description TEXT
 );
 
+CREATE TABLE IF NOT EXISTS project_images
+(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    mime_type VARCHAR(100) NOT NULL,
+    original_filename VARCHAR(255),
+    byte_size INT NOT NULL,
+    width INT,
+    height INT,
+    image_data MEDIUMBLOB NOT NULL,
+    created_at DATETIME DEFAULT NOW()
+);
+
 CREATE TABLE IF NOT EXISTS project_articles
 (
     id INT primary key auto_increment,
     userid int not null,
     postTime datetime DEFAULT NOW(),
     title VARCHAR(32),
-    content TEXT not null ,
-    image_path VARCHAR(64),
+    content TEXT not null,
+    image_id int(11) DEFAULT NULL,
     FOREIGN KEY (userid) REFERENCES project_users(id) ON DELETE CASCADE
+    FOREIGN KEY (image_id) REFERENCES project_images(id) ON DELETE SET NULL
 );
 
 CREATE TABLE IF NOT EXISTS project_comments
@@ -41,16 +54,4 @@ CREATE TABLE IF NOT EXISTS project_article_likes
     FOREIGN KEY (article_id) REFERENCES project_articles (id) ON DELETE CASCADE,
     FOREIGN KEY (userid) REFERENCES project_users (id) ON DELETE CASCADE,
     PRIMARY KEY (article_id, userid)
-);
-
-CREATE TABLE IF NOT EXISTS project_images
-(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    mime_type VARCHAR(100) NOT NULL,
-    original_filename VARCHAR(255),
-    byte_size INT NOT NULL,
-    width INT,
-    height INT,
-    image_data MEDIUMBLOB NOT NULL,
-    created_at DATETIME DEFAULT NOW()
 );
